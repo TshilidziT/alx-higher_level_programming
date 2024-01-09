a15d203abd07bf9515f4d77fde425db4ead7338a
TshilidziT <Tshilidzibytee@gmail.com>
2024-01-09 13:01:38 -0800

    try

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
+"""
+Module 11-student
+Contains class Student
+that initializes public instance attributes first_name, last_name, and age,
+and has public method to_json that retrieves its dictionary representation
+"""
 
-"""Defines a class Student."""
 
-
-class Student:
-    """Represent a student."""
-
+class Student():
+    """
+    Public Attributes:
+        first_name
+        last_name
+        age
+    Public Methods:
+        to_json: retrieves its dictionary representation
+    """
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+        """
+        Initializes student with full name and age
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Get a dictionary representation of the Student."""
-        return self.
+        """
+        Returns dictionary description with simple data structure
+        (list, dictionary, dictionary, string)
+        for JSON serialization of an object
+        """
+        return self.__dict__
